e136614061a019b904e4e0c9f3e8f8635f8a56fa
sridattayalla <sridatta555@gmail.com>
2020-05-16 11:08:08 +0530

    changed port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const http = require('http').Server(app)
 
 const io = require('socket.io')(http)
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname+ "/public"))
 
